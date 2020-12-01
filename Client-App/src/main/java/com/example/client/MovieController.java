4a57c9fb4b693dbcc01fe9eec32eb607483291bf
Mohammad Adnan Ataya <mohammadadnan.ataya@hiast.edu.sy>
2020-12-01 07:29:37 +0200

    updated

--- Client-App/src/main/java/com/example/client/MovieController.java
+++ Client-App/src/main/java/com/example/client/MovieController.java
@@ -5,12 +5,14 @@
  */
 package com.example.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,23 +21,26 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/movies")
-/*public class MovieController {
+public class MovieController {
+    
+    @Autowired
+    private ConsumeExternalAPI consumeExternalAPI;
     
     @Value("${api.key}")
     private String key;
     
-    @GetMapping("/id")
-    public Movie getMovie(@PathVariable("id") String id) {
-        return new Movie();
+    @GetMapping("/{id}")
+    public String getMovie(@RequestParam("id") String id) {
+        String movie = consumeExternalAPI.getMovie(id, key);
+        return movie;
     }
     
 }
 
-@FeignClient("https://api.themoviedb.org/3/movie/")
+@FeignClient(name = "consume-external-api", url = "https://api.themoviedb.org/3/movie")
 interface ConsumeExternalAPI {
     
-    @RequestMapping(method = RequestMethod.GET, value = id + "" + key)
-    Movie findById(@PathVariable("id") String id);
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}?api_key={key}")
+    String getMovie(@PathVariable("id") String id, @PathVariable("key") String key);
     
 }
-*/
